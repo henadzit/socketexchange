b6cc676382e0d32c0b80169ea9d5b05d5bfc575b
henadzit <vare6gin@gmail.com>
2013-10-22 08:31:13 +0100

    minor changes

--- webapp/eapp.py
+++ webapp/eapp.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 from flask import Flask, request, render_template, Response
 from gevent import monkey
 from socketio import socketio_manage
@@ -12,16 +13,27 @@ monkey.patch_all()
 
 
 class SessionNamespace(BaseNamespace, RoomsMixin, BroadcastMixin):
+    """TODO: specify packet formats
+    """
     def __init__(self, *args, **kwargs):
         super(SessionNamespace, self).__init__(*args, **kwargs)
-        # self.session['slug'] =
+        self.session['data'] = (datetime.min, None)
+        self.session['slug'] = ''
 
     def on_init(self, msg):
-        pkt = dict(type="event",
-                   name='change',
-                   args=msg)
-        print dir(request)
+        # slug = msg['slug']
+        slug = ''
+        last_data = self.session['data']
 
+        for session in self.__sessions(slug):
+            if session['data'][0] > last_data[0]:
+                last_data = session['data']
+
+        if last_data != self.session['data']:
+            pkt = dict(type="event")
+            self.socket.send_packet(pkt)
+
+        return True
 
     def on_change(self, msg):
         pkt = dict(type="event",
@@ -33,6 +45,10 @@ class SessionNamespace(BaseNamespace, RoomsMixin, BroadcastMixin):
                 socket.send_packet(pkt)
 
         return True
+
+    def __sessions(self, slug):
+        return (socket.session for sid, socket in self.socket.server.sockets.iteritems()
+                if self.socket != socket and socket.session['slug'] == slug)
 
 
 @app.route('/socket.io/<path:slug>')
